d65c7c9327a39b67bde95e4dfbf183ab0f26139f
artist0504 <cory0504@gmail.com>
2022-09-13 19:19:08 -0400

    display rock, paper, scissors picks and results from state

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 /* Imports */
 
 /* State */
+let gameState = 'pick';
+let pick = ''; // 'rock', 'paper', or 'scissors'
+let shoot = ''; // 'rock', 'paper', or 'scissors'
+let result = ''; // 'You Win!' or 'You Lose!'
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayPicks();
+    displayResults();
+    displayScoreboard();
+}
 
 /* Components */
 
 /* Component */
+/* Scoreboard */
+const winsDisplay = document.getElementById('win-display');
+const lossesDisplay = document.getElementById('losses-display');
+const drawDisplay = document.getElementById('draw-display');
+
+function displayScoreboard() {
+    winsDisplay.textContent = wins;
+    lossesDisplay.textContent = losses;
+    drawDisplay.textContent = wins - losses;
+}
+
 // get DOM
+const pickRock = document.getElementById('rock-pick');
+const pickPaper = document.getElementById('paper-pick');
+const pickScissors = document.getElementById('scissors-pick');
+
 // display
 // event listeners
 
+/* Results */
+const resultsSection = document.getElementById('results-section');
+const resultsSDisplay = document.getElementById('results-display');
+const playAgainButton = document.getElementById('play-again-button');
 /* Run page load code */
 loadPage();
